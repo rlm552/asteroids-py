23189715d8214f513a702d48921de2c7a4ed7850
Rory McGurty <rory.mcgurty@gmail.com>
2026-02-14 02:59:39 -0500

    Added forward and backward movement and groups

--- main.py
+++ main.py
@@ -4,26 +4,31 @@ from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from player import Player
 
 def main():
-    version = pygame.version.ver
-    print("Starting Asteroids with pygame version: " + version)
-    print("Screen width: " + str(SCREEN_WIDTH))
-    print("Screen height: " + str(SCREEN_HEIGHT))
-
+    
     pygame.init()
     clock = pygame.time.Clock()
     dt = 0
+    
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     x = SCREEN_WIDTH / 2
     y = SCREEN_HEIGHT / 2
     player = Player(x, y)
+    
+    
     while True:
         log_state()
-        player.update(dt)
+        #player.update(dt)
+        updatable.update(dt)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        player.draw(screen)
+        #player.draw(screen)
+        for drawables in drawable:
+            drawables.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60)/1000
         
